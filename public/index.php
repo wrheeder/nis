28886b3dc673d3c3e7c688ae83f5ac0b55e497ea
wrheeder <wrheeder@cellc.co.za>
2017-01-25 11:52:30 +0200

    public index

--- public/index.php
+++ public/index.php
@@ -5,3 +5,10 @@
  * Date: 25/01/2017
  * Time: 11:04 AM
  */
+
+date_default_timezone_set('Africa/Johannesburg');
+chdir('..');
+require_once'vendor/autoload.php';
+require_once 'lib/Frontend.php';
+$api=new Frontend("NIS","CellC");
+$api->main();
