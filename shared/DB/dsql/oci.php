27e696aa816958e4776bb8f2bfb5e74a6f3aa3f1
wrheeder <wrheeder@cellc.co.za>
2017-03-22 15:04:06 +0200

    ready

--- shared/DB/dsql/oci.php
+++ shared/DB/dsql/oci.php
@@ -5,3 +5,19 @@
  * Date: 23/02/2017
  * Time: 8:27 AM
  */
+class DB_dsql_oci extends DB_dsql
+{
+    public $bt = '';
+    public function limit($cnt, $shift = 0)
+    {
+        $cnt += $shift;
+        $this->where('NUM_ROWS>=', $shift);
+        $this->where('NUM_ROWS<', $cnt);
+
+        return $this;
+    }
+    public function render_limit()
+    {
+        return '';
+    }
+}
